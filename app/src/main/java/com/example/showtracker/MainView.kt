36f067bde45183f35fc7a59b9bdede2b9f7fa263
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-01-31 21:25:40 +0000

    Restructure Navigation to a separate file

--- app/src/main/java/com/example/showtracker/MainView.kt
+++ app/src/main/java/com/example/showtracker/MainView.kt
@@ -1,20 +1,13 @@
 package com.example.showtracker
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.showtracker.screens.Home
 
 
 @Composable
@@ -36,18 +29,7 @@ fun MainView() {
     }
 }
 
-@Composable
-fun Navigation(navController: NavController, viewModel: MainViewModel, pd:PaddingValues) {
 
-    NavHost(navController = navController as NavHostController, startDestination = Screen.Home.route, modifier = Modifier.padding(pd)) {
-        composable(Screen.Home.route) {
-            Home()
-        }
-        composable(Screen.WatchList.route) {
-
-        }
-    }
-}
 
 /*@Composable
 fun NavigationItem(
---
+++ app/src/main/java/com/example/showtracker/Navigation.kt
@@ -0,0 +1,24 @@
+package com.example.showtracker
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.showtracker.screens.Home
+
+@Composable
+fun Navigation(navController: NavController, viewModel: MainViewModel, pd: PaddingValues) {
+
+    NavHost(navController = navController as NavHostController, startDestination = Screen.Home.route, modifier = Modifier.padding(pd)) {
+        composable(Screen.Home.route) {
+            Home()
+        }
+        composable(Screen.WatchList.route) {
+
+        }
+    }
+}
