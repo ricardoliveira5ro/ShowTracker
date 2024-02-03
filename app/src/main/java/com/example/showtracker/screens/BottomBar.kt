3fd9e14b2ca1392ef19a160a8822255a7b2d352d
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-02-03 20:04:00 +0000

    Bottom bar change style

--- app/src/main/java/com/example/showtracker/screens/BottomBar.kt
+++ app/src/main/java/com/example/showtracker/screens/BottomBar.kt
@@ -1,32 +1,44 @@
 package com.example.showtracker.screens
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.showtracker.R
 import com.example.showtracker.screens
 
 @Composable
 fun BottomBar(currentRoute: String?, controller: NavController) {
-    BottomNavigation(Modifier.wrapContentSize()) {
+    BottomNavigation(
+        modifier = Modifier.wrapContentSize(),
+        backgroundColor = colorResource(id = R.color.blue_bottom_menu),
+        contentColor = Color.White
+    ) {
         screens.forEach {
             item ->
+                val selected = currentRoute == item.route
                 BottomNavigationItem(
-                    selected = currentRoute == item.route,
+                    selected = selected,
                     onClick = { controller.navigate(item.route) },
                     icon = {
-                        Icon(painter = painterResource(id = item.icon), contentDescription = item.title, modifier = Modifier.size(32.dp))
+                        Icon(
+                            painter = painterResource(id = item.icon),
+                            contentDescription = item.title,
+                            modifier = Modifier.size(26.dp),
+                            tint = if (selected) colorResource(id = R.color.pink) else colorResource(id = R.color.grey_button)
+                        )
                     },
-                    label = { Text(text = item.title) }
+                    modifier = Modifier.padding(vertical = 4.dp)
                 )
         }
     }
 }
-
