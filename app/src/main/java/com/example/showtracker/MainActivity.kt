f97bdabeb6e7bf06632816ce3907d5708640188b
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-02-07 17:41:49 +0000

    android status and navigation bar transparent & starting Show screen

--- app/src/main/java/com/example/showtracker/MainActivity.kt
+++ app/src/main/java/com/example/showtracker/MainActivity.kt
@@ -7,10 +7,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.core.view.WindowCompat
+import com.example.showtracker.screens.Show
 import com.example.showtracker.ui.theme.ShowTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = Color.Transparent.toArgb()
+        window.navigationBarColor = Color.Transparent.toArgb()
+
         super.onCreate(savedInstanceState)
         setContent {
             ShowTrackerTheme {
@@ -19,7 +27,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainView()
+                    Show()
+                    //MainView()
                 }
             }
         }
