7cc9a24ece7616a1b8d3d95f102fc8119843ba77
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2024-02-19 23:44:43 +0000

    Search Screen design

---
+++ app/src/main/java/com/example/showtracker/screens/Search.kt
@@ -0,0 +1,57 @@
+package com.example.showtracker.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.showtracker.fonts.Typography
+import com.example.showtracker.ui.theme.ShowTrackerTheme
+
+@Composable
+fun Search() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Explore",
+            color = Color.White,
+            fontFamily = Typography.openSans,
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+
+        SearchBar()
+        Spacer(modifier = Modifier.height(12.dp))
+
+        ShowList()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchPreview() {
+    ShowTrackerTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Search()
+        }
+    }
+}
